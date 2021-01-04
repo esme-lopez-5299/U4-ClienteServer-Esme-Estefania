0570593cbb28b3f41fd27f2ff709715c0ff5b187
esme-lopez-r2devpros <70729817+esme-lopez-r2devpros@users.noreply.github.com>
2021-01-04 14:57:06 -0600

    AVANCE.

---
+++ PokeApiProyectos/PokemonWPF/Helpers/BuscadorHelper.cs
@@ -0,0 +1,116 @@
+﻿using PokemonWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonWPF.Helpers
+{
+    public class BuscadorHelper : IEqualityComparer<PokemonInfo>
+    {
+        public bool Equals(PokemonInfo x, PokemonInfo y)
+        {
+            return x.id == y.id;
+        }
+
+        public int GetHashCode(PokemonInfo obj)
+        {
+            return obj.id.GetHashCode();
+        }
+
+        public static IEnumerable<PokemonInfo> FiltrarLista(IEnumerable<PokemonInfo> lista, string nombre, string habilidad, string tipo)
+        {
+            List<PokemonInfo> pokemones = new List<PokemonInfo>();
+
+            //Escribiste nombre?
+            if (nombre != null)
+            {
+                //Si hay nombre, escribiste habilidad?
+                if (habilidad != "--Abilities--")
+                {
+                    //Si hay habilidad, escribiste un tipo?
+                    if (tipo != "--Types--")
+                    {
+                        //Si hay tipo (Filtra por nombre, habilidad y tipo)
+                        foreach (var pokemon in lista)
+                        {
+                            foreach (var Habilidad in pokemon.abilities)
+                            {
+                                foreach (var tipos in pokemon.types)
+                                {
+                                    if (Habilidad.ability.name == habilidad && pokemon.name.Contains(nombre) && tipos.type.name == tipo)
+                                        pokemones.Add(pokemon);
+                                }
+                            }
+                        }
+                        return pokemones.Distinct(new BuscadorHelper());
+                    }
+                    else //No hay tipo, filtra por nombre y habilidad
+                    {
+                        foreach (var pokemon in lista)
+                        {
+                            foreach (var Habilidad in pokemon.abilities)
+                            {
+                                if (Habilidad.ability.name == habilidad && pokemon.name.Contains(nombre))
+                                    pokemones.Add(pokemon);
+                            }
+                        }
+                        return pokemones.Distinct(new BuscadorHelper());
+                    }
+
+                }
+                //No hay habilidad, Hay un tipo?
+                else if (tipo != "--Types--")
+                {
+                    //Si hay un tipo, se filtra por tipo y nombre
+                    foreach (var pokemon in lista)
+                    {
+                        foreach (var tipos in pokemon.types)
+                        {
+                            if (tipos.type.name == tipo && pokemon.name.Contains(nombre))
+                                pokemones.Add(pokemon);
+                        }
+                    }
+                    return pokemones.Distinct(new BuscadorHelper());
+                }
+
+                //No hay ni habilidad ni tipo, solo nombre
+                return lista;
+            }
+            else //No escribi nombre
+            {
+                //Hay habilidad?
+                if (habilidad != "--Abilities--")
+                {
+                    //Si hay habilidad, escribiste un tipo?
+                    if (tipo != "--Types--")
+                    {
+                        //Si hay tipo (Filtra por habilidad y tipo)
+                        foreach (var pokemon in lista)
+                        {
+                            foreach (var Habilidad in pokemon.abilities)
+                            {
+                                foreach (var tipos in pokemon.types)
+                                {
+                                    if (Habilidad.ability.name == habilidad && tipos.type.name == tipo)
+                                        pokemones.Add(pokemon);
+                                }
+                            }
+                        }
+                        return pokemones.Distinct(new BuscadorHelper());
+                    }
+                    else //No hay tipo filtra solo por habilidad
+                    {
+                        return lista;
+                    }
+                }
+                //No hay habilidad, solo hay tipo
+                else
+                {
+                    return lista;
+                }
+            }
+        }
+    }
+}
---
+++ PokeApiProyectos/PokemonWPF/ViewModels/PokemonViewModel.cs
@@ -0,0 +1,131 @@
+﻿using GalaSoft.MvvmLight.Command;
+using Newtonsoft.Json;
+using PokemonWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using static PokemonWPF.Models.ability;
+
+namespace PokemonWPF.ViewModels
+{
+    public class PokemonViewModel : INotifyPropertyChanged
+    {
+
+        public class Busqueda
+        {
+            public string habilidadBuscada { get; set; }
+            public string tipoBuscado { get; set; }
+            public string nombreBuscado { get; set; }
+        }
+
+        public Busqueda BusquedaObj { get; set; } = new Busqueda();
+        public ObservableCollection<PokemonInfo> Pokemones { get; set; }
+        public abilityInfo Habilidades { get; set; }        
+        public TypesInfo Tipos { get; set; }
+        public string Mensaje { get; set; }
+        
+
+       
+        public ICommand HacerBusquedaCommand { get; set; }
+
+        HttpClient client = new HttpClient() { BaseAddress = new Uri("https://pokeapi.co/api/v2/") };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Lanzar(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }     
+        
+        async void GetCombos()
+        {
+            var result = await client.GetAsync("ability?limit=327");
+            var tipos = await client.GetAsync("type");
+
+            if (result.IsSuccessStatusCode&&tipos.IsSuccessStatusCode)
+            {
+                var jsonHabilidades = await result.Content.ReadAsStringAsync();
+                var jsonTipos = await tipos.Content.ReadAsStringAsync();
+
+                Habilidades = JsonConvert.DeserializeObject<abilityInfo>(jsonHabilidades);
+                Tipos = JsonConvert.DeserializeObject<TypesInfo>(jsonTipos);
+                Lanzar();
+            }
+        }
+
+        public PokemonViewModel()
+        {
+            GetCombos();
+            HacerBusquedaCommand = new RelayCommand<Busqueda>(HacerBusqueda);
+        }
+
+        private async void HacerBusqueda(Busqueda parametrosBusqueda)
+        {
+
+            try
+            {
+                ObservableCollection<PokemonInfo> pokemonesTemp = new ObservableCollection<PokemonInfo>();
+                if (!String.IsNullOrEmpty(parametrosBusqueda.nombreBuscado))
+                {
+                    var result = await client.GetAsync($"pokemon/{parametrosBusqueda.nombreBuscado.ToLower()}");
+
+                    if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var json = await result.Content.ReadAsStringAsync();
+                        PokemonInfo p = JsonConvert.DeserializeObject<PokemonInfo>(json);
+                        pokemonesTemp.Add(p);
+
+                    }
+                    else
+                    {
+                        Pokemones = pokemonesTemp;
+                    }
+                }
+                //Busca los de la habilidad
+                if (parametrosBusqueda.habilidadBuscada.ToString() != "--Abilities--")
+                {
+                    var resultHabilidad = await client.GetAsync($"ability/{parametrosBusqueda.habilidadBuscada}");
+                    var jsonHabilidad = await resultHabilidad.Content.ReadAsStringAsync();
+                    HabilidadCompletaInfo habilidadObtenida = JsonConvert.DeserializeObject<HabilidadCompletaInfo>(jsonHabilidad);
+
+                    foreach (var p in habilidadObtenida.pokemon)
+                    {
+                        var poke = await client.GetAsync($"pokemon/{p.pokemon.name}");
+                        var jsonPoke = await poke.Content.ReadAsStringAsync();
+                        PokemonInfo poke2 = JsonConvert.DeserializeObject<PokemonInfo>(jsonPoke);
+                        pokemonesTemp.Add(poke2);
+                    }
+                }
+                //Buscar por tipos
+                if (parametrosBusqueda.tipoBuscado != "--Types--")
+                {
+                    var resultTipo = await client.GetAsync($"type/{parametrosBusqueda.tipoBuscado }");
+                    var jsonTipo = await resultTipo.Content.ReadAsStringAsync();
+                    TipoCompletoInfo tipoObtenido = JsonConvert.DeserializeObject<TipoCompletoInfo>(jsonTipo);
+
+                    foreach (var p in tipoObtenido.pokemon)
+                    {
+                        var poke = await client.GetAsync($"pokemon/{p.pokemon.name}");
+                        var jsonPoke = await poke.Content.ReadAsStringAsync();
+                        PokemonInfo poke2 = JsonConvert.DeserializeObject<PokemonInfo>(jsonPoke);
+                        pokemonesTemp.Add(poke2);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Mensaje = e.ToString();
+                Lanzar(nameof(Mensaje));
+            }
+          
+        }
+    }
+
+    }
+
